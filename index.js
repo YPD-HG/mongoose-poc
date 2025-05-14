1400f2654088cc907a1e32cf494c122583c6d3ae
Yashpriyadeep Katta <yashpriyadeepkatta@Yashpriyadeeps-MacBook-Air.local>
2025-05-14 17:29:21 +0530

    security/input-validation-zod+done+timestamp

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
+
 const express = require('express')
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
+const { z } = require('zod')
 const { UserModel, TodoModel } = require('./db')
 const app = express()
 JWT_SECRET = "yash2000deep"
@@ -11,13 +14,32 @@ mongoose.connect('mongodb+srv://yashdeep:yashdeep2000@cluster0.vlrglmq.mongodb.n
 app.use(express.json())
 
 app.post('/signup', async function (req, res) {
+
+    const requiredBody = z.object({
+        email: z.string().min(3).max(100).email(),
+        name: z.string().min(3).max(100),
+        password: z.string().min(3).max(30).regex(new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$'), {
+            message:
+                'Password must be at least 8 characters and contain an uppercase letter, lowercase letter, and number'
+        })
+    }) // Schema defined using Zod
+
+    // Parse the input data based on the schema
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+
+    if (!parsedDataWithSuccess.success) {
+        res.json({
+            message: "Incorrect Format",
+            error: parsedDataWithSuccess.error
+        })
+        return;
+    }
     const email = req.body.email
     const password = req.body.password
     const name = req.body.name
     let errorFound = 0;
     try {
         let hashPassword = await bcrypt.hash(password, 5)
-        console.log("HashPassword :", hashPassword)
         await UserModel.create({
             email: email,
             password: hashPassword,
@@ -53,7 +75,7 @@ app.post('/signin', async function (req, res) {
         } else {
             const token = jwt.sign({
                 id: user._id.toString()
-            }, JWT_SECRET)
+            }, process.env.JWT_SECRET)
             res.json({
                 token
             })
@@ -85,10 +107,14 @@ app.post('/todo', auth, async function (req, res) {
     let title = req.body.title;
     let done = req.body.done;
 
+    const now = new Date();
+    const time = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true });
+
     await TodoModel.create({
         title,
         done,
-        userId
+        userId,
+        time
     })
 
     res.json({
@@ -97,6 +123,20 @@ app.post('/todo', auth, async function (req, res) {
         userId,
         message: "Data filled in database."
     })
+})
+
+app.post('/todo_done', auth, async function (req, res) {
+    let errorFound = 0;
+    try {
+        await TodoModel.findById(req.headers.id).updateOne({ $set: { done: true } });
+    } catch (e) {
+        errorFound = 1;
+        console.log(e)
+    }
+    if (!errorFound)
+        res.json({
+            message: "Task Completed!"
+        })
 })
 
 app.get('/todos', auth, async function (req, res) {
